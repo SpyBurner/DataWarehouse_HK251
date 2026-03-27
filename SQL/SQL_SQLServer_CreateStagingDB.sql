ee42097a1b05af13397c3b6a591c1d84a033cb08
SpyBurner <zombiepigman1505@gmail.com>
2026-03-27 22:26:03 +0700

    Pentaho staging - Auto-create SQL Server Staging table - Generate Schema.txt file

--- SQL/SQL_SQLServer_CreateStagingDB.sql
+++ SQL/SQL_SQLServer_CreateStagingDB.sql
@@ -1,14 +1,7 @@
 USE master;
 GO
 
--- Forcefully close any existing connections and drop the database
-IF EXISTS (SELECT * FROM sys.databases WHERE name = 'DW_Staging')
-BEGIN
-    ALTER DATABASE DW_Staging SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DW_Staging;
-END
-GO
-
 -- Create the fresh database
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'DW_Staging')
 CREATE DATABASE DW_Staging;
 GO
